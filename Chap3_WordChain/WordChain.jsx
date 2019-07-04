62270389d9f8364f1db2a204daeaf1c1e0e628bf
hyukjung <hyukjung1013@gmail.com>
2019-07-04 20:26:04 +0900

    [Chap3] 3rd commit: React component ‘WordChain’ was implemented.

--- Chap3_WordChain/WordChain.jsx
+++ Chap3_WordChain/WordChain.jsx
@@ -4,11 +4,44 @@ const { Component } = React;
 class WordChain extends Component {
 
     state = {
-        text: 'Hello, webpack.'
+        word: '시작',
+        input: '',
+        result: '',
     };
 
+    onChangeInput = (e) => {
+        this.setState( { input: e.target.value } );
+    }
+
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        if ( this.state.word[ this.state.word.length -1 ] === this.state.input[0]) {
+            this.setState({
+                result: 'Correct',
+                word: this.state.input,
+                input: '',
+            });
+            this.inputRef.focus();
+        } else {
+            this.setState({
+                result: 'Incorrect',
+                input: '',
+            });
+            this.inputRef.focus();
+        }
+    }
+
+    inputRef;
+
     render() {
-        return <div>{this.state.text}</div>
+        return <>
+            <div>{this.state.word}</div>
+            <form onSubmit={this.onSubmitForm}>
+                <input ref={(c) => { this.inputRef = c }} value={this.state.input} onChange={this.onChangeInput} />
+                <button>입력</button>
+            </form>
+            <div>{this.state.result}</div>
+        </>
     }
 }
 
