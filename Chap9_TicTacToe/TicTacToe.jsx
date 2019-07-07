832c259a39f743e12533187e0da6122dafb7b1b0
hyukjung <hyukjung1013@gmail.com>
2019-07-08 00:18:16 +0900

    [Chap9] 1st commit: (1) How to use ‘useReducer()’. (2) How to get an index of the clicked cell. (3) ‘Turn over’ function was implemented.

---
+++ Chap9_TicTacToe/TicTacToe.jsx
@@ -0,0 +1,61 @@
+import React, { useReducer, useCallback } from 'react';
+import Table from './Table'
+
+const initialState = {
+  winner: '',
+  turn: 'O',
+  tableData: [
+    ['', '', ''], 
+    ['', '', ''], 
+    ['', '', '']
+  ]
+}
+
+// Action objects.
+export const SET_WINNER = 'SET_WINNER'
+export const CLICK_CELL = 'CLICK_CELL'
+export const CHANGE_TURN = 'CHANGE_TURN'
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case SET_WINNER: 
+      return {          // Create a new object.
+        ...state,
+        winner: action.winner
+      };
+    case CLICK_CELL:
+      const tableData = [...state.tableData]
+      tableData[action.row] = [...tableData[action.row]];
+      tableData[action.row][action.cell] = state.turn;
+      return {
+        ...state,
+        tableData,
+      }
+    case CHANGE_TURN: 
+      return {
+        ...state,
+        turn: state.turn === 'O' ? 'X' : 'O'
+      }
+  }
+}
+
+const TicTacToe = () => {
+
+  // const [winner, setWinner] = useState('');
+  // const [turn, setTurn] = useState('O');
+  // const [tableData, setTableData] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const onClickTable = useCallback( () => {
+    dispatch({ type: SET_WINNER, winner: 'O'})  // This calls reducer.
+  }, []);
+
+  return (
+    <>
+      <Table onClick={onClickTable} tableData={state.tableData} dispatch={dispatch}/>
+      {state.winner && <div>{state.winner}님의 승리</div>}
+    </>
+  )
+}
+
+export default TicTacToe; 
